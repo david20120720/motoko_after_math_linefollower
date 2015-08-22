2251578d416c462e5f1d8771b0415897cc78f952
michalnand <michal.nand@gmail.com>
2015-08-22 07:22:02 +0200

    mess cleanded up

--- src/current/usr/line_follower.c
+++ src/current/usr/line_follower.c
@@ -1,25 +1,25 @@
 #include "line_follower.h"
 
-#include "../lib_usr/line_sensor.h"	
-#include "../lib_usr/math.h" 
-#include "pid.h" 
+#include "../lib_usr/line_sensor.h"
+#include "../lib_usr/math.h"
+#include "pid.h"
 #include "fuzzy_mux.h"
 #include "predictor.h"
 
 struct sPID g_line_pid_0, g_line_pid_1;
- 
+
 void line_follower_init()
 {
 	g_line_follower.base_speed = 1.0;
 	g_line_follower.base_speed_ = 0.0;
 	g_line_follower.dif_speed = 0.0;
-  
+
 
 	//POLOLU WHEELS 32MM
 
 	//controller for straight line
 	float b0 = 0.09211;
-	float a0 = 0.98; 
+	float a0 = 0.98;
 
 	//controller for curved line
 	float b1 = 0.09211;
@@ -32,9 +32,9 @@ void line_follower_init()
 	pid_init(&g_line_pid_1, SPEED_MAX, 0.0, 0.0, 0.0, 0.0);
 	pid_synthetise(&g_line_pid_1, 1.0/b1, -(a1 + 1.0)/b1, a1/b1, a2/b1);
 
- 
+
 	predictor_init();
-}   
+}
 
 void line_follower_main()
 {
@@ -47,24 +47,24 @@ void line_follower_main()
 
 
 	g_line_follower.base_speed = 0.7;
-	
-	g_line_follower.base_speed = (1.0*predictor_process(g_line_sensor.line_position))/SPEED_MAX; 
 
-	//float ks = 0.007; 
+	g_line_follower.base_speed = (1.0*predictor_process(g_line_sensor.line_position))/SPEED_MAX;
+
+	//float ks = 0.007;
 	//float ks = 0.008;
 	float ks = 0.01;
-	
 
-	 
+
+
 	g_line_follower.base_speed_ = m_min(g_line_follower.base_speed_ + tmp*ks, tmp);
-	g_line_follower.base_speed_ = m_saturate(g_line_follower.base_speed_, 0.4, 1.2*g_line_follower.base_speed);
+	g_line_follower.base_speed_ = m_saturate(g_line_follower.base_speed_, 0.4, 0.9*g_line_follower.base_speed);
 
-		
-	//g_line_follower.base_speed_ = 0.4;  
- 
+
+	//g_line_follower.base_speed_ = 0.4;
+
 	i32 motor_right = (g_line_follower.base_speed_ + g_line_follower.dif_speed)*SPEED_MAX;
 	i32 motor_left = (g_line_follower.base_speed_ - g_line_follower.dif_speed)*SPEED_MAX;
 
-	
-	drv8834_run(motor_left, motor_right); 
-}   
+
+	drv8834_run(motor_left, motor_right);
+}
